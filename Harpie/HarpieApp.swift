59a7065b4ea3e04a07774027cc2ea0eb1c0cf155
Gerardo Gallegos <36527267+gallegosg@users.noreply.github.com>
2024-12-08 14:50:31 -0800

    Initial Commit

---
+++ Harpie/HarpieApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarpieApp.swift
+//  Harpie
+//
+//  Created by Gerardo Gallegos on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HarpieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarpieTests/HarpieTests.swift
@@ -0,0 +1,17 @@
+//
+//  HarpieTests.swift
+//  HarpieTests
+//
+//  Created by Gerardo Gallegos on 12/8/24.
+//
+
+import Testing
+@testable import Harpie
+
+struct HarpieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
